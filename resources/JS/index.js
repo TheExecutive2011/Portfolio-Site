a7f98fae553abd05d4d5f341bf972374cc4182ac
TheExecutive2011 <WarrenMorrison@Outlook.com>
2023-01-04 17:21:55 -0500

    Console logging test.

--- resources/JS/index.js
+++ resources/JS/index.js
@@ -1,13 +1,14 @@
 const g_maps_url = "https://portfolio-site-back-end.herokuapp.com/api/maps"
 
 async function getGMaps() {
-    fetch(g_maps_url)
-  .then(response => response.text())
-  .then(iframe => {
-    document.getElementById('gMaps').innerHTML = iframe;
-  })
-  .catch(error => {
-    console.error(error);
-  });
+  console.log('getGMaps() reached successfully')
+  fetch(g_maps_url)
+    .then(response => response.text())
+    .then(iframe => {
+      document.getElementById('gMaps').innerHTML = iframe;
+    })
+    .catch(error => {
+      console.error(error);
+    });
 }
 
