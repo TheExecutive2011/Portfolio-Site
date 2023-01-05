25c7161482a112811692df8ec71043805b7ba66a
TheExecutive2011 <WarrenMorrison@Outlook.com>
2023-01-04 19:58:23 -0500

    Test lines.

--- resources/JS/index.js
+++ resources/JS/index.js
@@ -3,11 +3,13 @@ const g_maps_url = "https://portfolio-site-back-end.herokuapp.com/api/maps"
 async function getGMaps() {
   console.log('getGMaps() reached successfully')
   fetch(g_maps_url)
+    console.log('fetch reached successfully')
     .then(response => response.text())
     .then(iframe => {
       document.getElementById('gMaps').innerHTML = iframe;
     })
     .catch(error => {
+      console.log('error reached successfully')
       console.error(error);
     });
 }
