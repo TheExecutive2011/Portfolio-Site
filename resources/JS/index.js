6f80b5c10c0457a98fcfa2088b5a52a82260dd86
TheExecutive2011 <WarrenMorrison@Outlook.com>
2023-01-04 22:07:33 -0500

    Modified getGMaps() method.

--- resources/JS/index.js
+++ resources/JS/index.js
@@ -1,16 +1,15 @@
 const g_maps_url = "https://portfolio-site-back-end.herokuapp.com/api/maps"
 
 async function getGMaps() {
-  console.log('getGMaps() reached successfully')
-  fetch(g_maps_url)
-    console.log('fetch reached successfully')
-    .then(response => response.text())
-    .then(iframe => {
-      document.getElementById('gMaps').innerHTML = iframe;
-    })
-    .catch(error => {
-      console.log('error reached successfully')
-      console.error(error);
-    });
+  try {
+    console.log('getGMaps() try reached successfully')
+    const response = await fetch(g_maps_url);
+    const iframe = await response.text();
+    document.getElementById('gMaps').innerHTML = iframe;
+  } 
+  catch (error) {
+    console.log('getGMaps() catch reached...successfully?')
+    console.error(error);
+  }
 }
 
