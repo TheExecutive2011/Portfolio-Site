20fcf2ce2fc2643a5fac67e8c23cb7d8981e92a6
TheExecutive2011 <WarrenMorrison@Outlook.com>
2023-01-09 14:05:24 -0500

    Added button functionality

--- resources/JS/index.js
+++ resources/JS/index.js
@@ -6,10 +6,35 @@ async function getGMaps() {
     const response = await fetch(g_maps_url);
     const iframe = await response.text();
     document.getElementById('gMaps').innerHTML = iframe;
-  } 
+  }
   catch (error) {
-    console.log('getGMaps() catch reached...successfully?')
+    console.log('getGMaps() error catch reached...successfully?')
     console.error(error);
   }
 }
 
+async function getResume() {
+  try {
+    //Fetches document from the back end
+    fetch("resources/images/WarrenMorrison_Resume_1-7-2023.pdf").then(function (response) {
+      response.blob().then(function (blob) {
+        //Creates a new Blob object with the resume file data
+        const url = URL.createObjectURL(blob);
+        //Creates a new anchor element
+        const a = document.createElement("a");
+        //Sets the href attribute of the anchor element to the URL of the PDF file
+        a.href = url;
+        //Sets the download attribute of the anchor element
+        a.download = "resources/images/WarrenMorrison_Resume_1-7-2023.pdf";
+        //Simulates a mouse click on the anchor element
+        a.click();
+        // revoke the URL object to free up memory
+        URL.revokeObjectURL(url);
+      });
+    });
+  }
+  catch (error) {
+    console.log('getResume() error catch reached...successfully?')
+    console.error(error);
+  }
+}
